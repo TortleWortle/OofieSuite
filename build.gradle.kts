8e6c5aa9bd1901c945297728882c5f0586ed46e1
TortleWortle <tortlewortle@gmail.com>
2021-01-22 18:05:12 +0100

    always copy paste random code without checking if it's correct

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ tasks {
 gradle.buildFinished {
     project.buildDir.deleteRecursively()
 }
-val v = System.getProperty("OOFIE_VERSION") ?: "DEV"
+val v = System.getenv("OOFIE_VERSION") ?: "DEV"
 
 subprojects {
     val subProject = this.project
